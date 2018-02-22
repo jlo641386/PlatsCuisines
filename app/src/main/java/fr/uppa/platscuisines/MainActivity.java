eccb771dc683da25a5230880fb596fbcbca8ebd9
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-22 14:43:05 +0100

    Validate the Dish information

--- app/src/main/java/fr/uppa/platscuisines/MainActivity.java
+++ app/src/main/java/fr/uppa/platscuisines/MainActivity.java
@@ -3,9 +3,11 @@ package fr.uppa.platscuisines;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import fr.uppa.platscuisines.models.Dish;
 import fr.uppa.platscuisines.models.DishBuilder;
@@ -26,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
         Switch packof6 = findViewById(R.id.packof6);
         Switch packof8 = findViewById(R.id.packof8);
         Switch packof12 = findViewById(R.id.packof12);
+
+        boolean error = false;
+        String errorMsg = null;
+
+        if (dishName.getText().toString().isEmpty()){
+            errorMsg = "Enter the Dish's name";
+            dishName.requestFocus();
+            error=true;
+        } else if(!packof2.isChecked() && !packof4.isChecked() && !packof6.isChecked() &&
+                !packof8.isChecked() && !packof12.isChecked()){
+            errorMsg = "Select at least a pack presentation";
+            dishName.requestFocus();
+            error = true;
+        }
+
+        if(error){
+            Toast toast = Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+
+            return;
+        }
 
         Dish dish = new DishBuilder().setNomPlat(dishName.getText().toString()).
                 setConditionnePour2(packof2.isChecked()?1:0).
