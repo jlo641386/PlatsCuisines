6285bc95b7f361b15191b322bbc5984379ec53bf
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-22 12:19:26 +0100

    Created the dish activity

---
+++ app/src/main/java/fr/uppa/platscuisines/DisplayDishActivity.java
@@ -0,0 +1,13 @@
+package fr.uppa.platscuisines;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayDishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_dish);
+    }
+}
