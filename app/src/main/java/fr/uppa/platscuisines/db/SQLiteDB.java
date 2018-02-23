fb168e7fa6f201170650387caeaf405cfc60234b
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-23 11:04:02 +0100

    SQLiteDB class as a Singleton

--- app/src/main/java/fr/uppa/platscuisines/db/SQLiteDB.java
+++ app/src/main/java/fr/uppa/platscuisines/db/SQLiteDB.java
@@ -1,11 +1,26 @@
 package fr.uppa.platscuisines.db;
 
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+
+import fr.uppa.platscuisines.MyApp;
 
 /**
  * Created by mbsanchez on 22/02/2018.
  */
 
 public abstract class SQLiteDB extends RoomDatabase {
+    private static SQLiteDB instance = null;
 
+    public static synchronized SQLiteDB getInstance() {
+        if (instance == null) {
+            instance = Room.databaseBuilder(MyApp.getAppContext(),
+                    SQLiteDB.class,
+                    "bdplatscuisines.db")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+
+        return instance;
+    }
 }
