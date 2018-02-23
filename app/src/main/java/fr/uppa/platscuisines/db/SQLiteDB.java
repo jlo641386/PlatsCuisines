ef298c5100eda2a3ac4167c383b48cc7c7c24c99
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-24 00:36:35 +0100

    Customize the DataSource

--- app/src/main/java/fr/uppa/platscuisines/db/SQLiteDB.java
+++ app/src/main/java/fr/uppa/platscuisines/db/SQLiteDB.java
@@ -1,14 +1,17 @@
 package fr.uppa.platscuisines.db;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 
 import fr.uppa.platscuisines.MyApp;
+import fr.uppa.platscuisines.models.Dish;
 
 /**
  * Created by mbsanchez on 22/02/2018.
  */
 
+@Database(entities = Dish.class, version = 1)
 public abstract class SQLiteDB extends RoomDatabase {
     private static SQLiteDB instance = null;
 
