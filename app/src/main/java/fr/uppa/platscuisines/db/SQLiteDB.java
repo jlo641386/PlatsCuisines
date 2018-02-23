5677dc170b31568b5f92c734468ee4a5ac831ec1
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-23 10:32:59 +0100

    Inherit SQLiteDB from RoomDatabase

--- app/src/main/java/fr/uppa/platscuisines/db/SQLiteDB.java
+++ app/src/main/java/fr/uppa/platscuisines/db/SQLiteDB.java
@@ -1,8 +1,11 @@
 package fr.uppa.platscuisines.db;
 
+import android.arch.persistence.room.RoomDatabase;
+
 /**
  * Created by mbsanchez on 22/02/2018.
  */
 
-public class SQLiteDB {
+public abstract class SQLiteDB extends RoomDatabase {
+
 }
