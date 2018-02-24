3e222f2dbc77d1d3f0a0ec9da0445094b205678b
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-24 23:42:37 +0100

    Added the Component interface

---
+++ app/src/main/java/fr/uppa/platscuisines/DataComponent.java
@@ -0,0 +1,15 @@
+package fr.uppa.platscuisines;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by mbsanchez on 24/02/2018.
+ */
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface DataComponent {
+    void inject(MainActivity mainActivity);
+}
