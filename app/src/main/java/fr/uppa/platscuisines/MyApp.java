ac9ac947adfddf7c98832be649e31865ba504047
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-25 00:09:31 +0100

    Settled up the Application class for Dependency Injection

--- app/src/main/java/fr/uppa/platscuisines/MyApp.java
+++ app/src/main/java/fr/uppa/platscuisines/MyApp.java
@@ -9,14 +9,20 @@ import android.content.Context;
 
 public class MyApp extends Application {
     private static Context appContext;
+    private DataComponent dataComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
         appContext = getApplicationContext();
+        dataComponent = DaggerDataComponent.builder().appModule(new AppModule(this)).build();
     }
 
     public static Context getAppContext() {
         return appContext;
+    }
+
+    public DataComponent getDataComponent() {
+        return this.dataComponent;
     }
 }
