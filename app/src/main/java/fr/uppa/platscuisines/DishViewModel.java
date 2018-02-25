4479797e1e2ce4c7c4b48150a282b231369b1dd0
mbsanchez <mbsanchez@unet.edu.ve>
2018-02-25 22:56:27 +0100

    added wrappers methods for conditionnePourX attributes.

--- app/src/main/java/fr/uppa/platscuisines/DishViewModel.java
+++ app/src/main/java/fr/uppa/platscuisines/DishViewModel.java
@@ -24,4 +24,49 @@ public class DishViewModel extends BaseObservable {
     public Dish getDish(){
         return dish;
     }
+
+    public boolean getConditionnePour2() {
+        return dish.getConditionnePour2()!=0;
+    }
+
+    public void setConditionnePour2(boolean conditionnePour2) {
+        dish.setConditionnePour2(conditionnePour2?1:0);
+        notifyChange();
+    }
+
+    public boolean getConditionnePour4() {
+        return dish.getConditionnePour4()!=0;
+    }
+
+    public void setConditionnePour4(boolean conditionnePour4) {
+        dish.setConditionnePour4(conditionnePour4?1:0);
+        notifyChange();
+    }
+
+    public boolean getConditionnePour6() {
+        return dish.getConditionnePour6()!=0;
+    }
+
+    public void setConditionnePour6(boolean conditionnePour6) {
+        dish.setConditionnePour6(conditionnePour6?1:0);
+        notifyChange();
+    }
+
+    public boolean getConditionnePour8() {
+        return dish.getConditionnePour8()!=0;
+    }
+
+    public void setConditionnePour8(boolean conditionnePour8) {
+        dish.setConditionnePour8(conditionnePour8?1:0);
+        notifyChange();
+    }
+
+    public boolean getConditionnePour12() {
+        return dish.getConditionnePour12()!=0;
+    }
+
+    public void setConditionnePour12(boolean conditionnePour12) {
+        dish.setConditionnePour12(conditionnePour12?1:0);
+        notifyChange();
+    }
 }
