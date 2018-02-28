21066f84959c09e1d5fd9e98b257cd278cc895ee
mbsanchez <mbsanchez@unet.edu.ve>
2018-03-01 00:35:04 +0100

    Put the Dishes into the RecyclerView

--- app/src/main/java/fr/uppa/platscuisines/DishViewActivity.java
+++ app/src/main/java/fr/uppa/platscuisines/DishViewActivity.java
@@ -24,5 +24,17 @@ public class DishViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish_view);
+        ((MyApp)getApplication()).getDataComponent().inject(this);
+
+        List<Dish> dishes = dishDAO.getAll();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new DishItemAdapter(getApplicationContext(), dishes);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
