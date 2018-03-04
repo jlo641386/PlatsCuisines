8fc128eed9f839218db931bea4ac1b48df0c0346
mbsanchez <mbsanchez@unet.edu.ve>
2018-03-04 22:26:51 +0100

    provide the Façade class as a Singleton dependency

--- app/src/main/java/fr/uppa/platscuisines/AppModule.java
+++ app/src/main/java/fr/uppa/platscuisines/AppModule.java
@@ -3,10 +3,13 @@ package fr.uppa.platscuisines;
 import android.app.Application;
 import android.content.Context;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 import fr.uppa.platscuisines.db.DishDAO;
 import fr.uppa.platscuisines.db.SQLiteDB;
+import fr.uppa.platscuisines.models.SaveOrderFacade;
 
 /**
  * Created by mbsanchez on 24/02/2018.
@@ -28,5 +31,11 @@ public class AppModule {
     @Provides
     public DishDAO provideDishDAO(){
         return SQLiteDB.getInstance().getDishDao();
+    }
+
+    @Provides
+    @Singleton
+    public SaveOrderFacade provideSaveOrderFacade(){
+        return new SaveOrderFacade();
     }
 }
