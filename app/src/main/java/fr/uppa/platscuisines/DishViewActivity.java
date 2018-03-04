358a172215b471f22ca985cd9224b030d3a221be
mbsanchez <mbsanchez@unet.edu.ve>
2018-03-04 23:51:11 +0100

    Added handler methods for ADD dish to order and Send order buttons

--- app/src/main/java/fr/uppa/platscuisines/DishViewActivity.java
+++ app/src/main/java/fr/uppa/platscuisines/DishViewActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -11,10 +14,15 @@ import javax.inject.Inject;
 
 import fr.uppa.platscuisines.db.DishDAO;
 import fr.uppa.platscuisines.models.Dish;
+import fr.uppa.platscuisines.models.DishOrderItemBuilder;
+import fr.uppa.platscuisines.models.SaveOrderFacade;
 
 public class DishViewActivity extends AppCompatActivity {
     @Inject
     DishDAO dishDAO;
+
+    @Inject
+    SaveOrderFacade saveOrderFacade;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -36,5 +44,40 @@ public class DishViewActivity extends AppCompatActivity {
         // specify an adapter (see also next example)
         mAdapter = new DishItemAdapter(getApplicationContext(), dishes);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void addDishToOrder(View view){
+        int platId = (int) view.getTag();
+        Dish dish = dishDAO.findById(platId);
+
+        if(dish == null){
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Error, the dish doesn't exist in the database!", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+            return;
+        }
+
+        saveOrderFacade.addItem(new DishOrderItemBuilder().setId(dish.getIdPlat()).
+                setDishName(dish.getNomPlat()).setQuantityFor2(1).setQuantityFor4(1).
+                setQuantityFor6(1).setQuantityFor8(1).setQuantityFor12(1).createDishOrderItem());
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                dish.getNomPlat()+ ", added!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void sendOrder(View view){
+        if(saveOrderFacade.orderSize()<=0){
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Not dishes have yet been added to the order!", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+            return;
+        }
+
+        saveOrderFacade.saveOrder(getApplicationContext());
+        saveOrderFacade.sendOrder(getApplicationContext());
+        saveOrderFacade.clearOrder();
     }
 }
